ee8b5c64b414f7f0ec7f1541345d104e67dfb18a
Victor Guerrero Bosch <vguerrerobosch@gmail.com>
2020-06-11 02:52:45 +0200

    fix clone path

--- clone.sh
+++ clone.sh
@@ -6,7 +6,7 @@ SITES=$HOME/Sites
 
 # Personal
 git clone git@github.com:vguerrerobosch/vguerrerobosch.com.git $SITES/vguerrerobosch.com
-git clone git@github.com:vguerrerobosch/estudifar.git
+git clone git@github.com:vguerrerobosch/estudifar.git $SITES/estudifar
 
 # Clients
 git clone git@gitlab.com:vguerrerobosch/alreves.git $SITES/alreves
